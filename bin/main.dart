2054b28ef0afcd11d5ff836557df3ebba074e847
bernardkkt <kianshunkin@gmail.com>
2020-05-20 18:33:50 +0800

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:SeriousJoyfulCopyleft/SeriousJoyfulCopyleft.dart' as SeriousJoyfulCopyleft;
+
+main(List<String> arguments) {
+  print('Hello world: ${SeriousJoyfulCopyleft.calculate()}!');
+}
